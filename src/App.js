238f490273f9d1b40b70ed63777a3f1507656205
marko <mark.opp.1989@gmail.com>
2021-05-16 10:48:15 +0900

    Setup Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Switch} from 'react-router-dom';
+import Header from "./components/Header";
+import Home from './sites/Home';
+import About from './sites/About';
+import Intro from "./sites/Intro";
+import Places from "./sites/Places";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Header/>
+            <Switch>
+                <Route exact path="/">
+                    <Home/>
+                </Route>
+                <Route exact path="/about">
+                    <About/>
+                </Route>
+                <Route exact path="/intro">
+                    <Intro/>
+                </Route>
+                <Route exact path="/places">
+                    <Places/>
+                </Route>
+            </Switch>
+        </>
+    );
 }
 
 export default App;
